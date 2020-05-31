e3ad2edac086d3faedf996c8e2c5aa8a13d4d652
ejaramillom <ejaramilo@hotmail.com>
2020-05-31 16:17:19 -0500

    new config for deplyoment

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const mongoose = require( 'mongoose' );
 const path = require( 'path' );
 const User = require( './models/User' );
 const Poll = require( './models/Poll' );
+const compression = require( 'compression' );
 const routes = require( './routes' );
 const flash = require( 'connect-flash' );
 
-mongoose.connect( process.env.MONGODB_URL || 'mongodb://localhost:27017/encuestas', { useNewUrlParser: true } );
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/encuestas', { useNewUrlParser: true } );
+mongoose.set( 'useFindAndModify', false);
 
 const app = express();
 app.set( 'view engine', 'pug' );
@@ -22,8 +24,10 @@ app.use( function( req, res, next ){
   res.locals.message = req.flash();
   next();
 });
+
 app.use( express.static(__dirname + '../public'));
 app.use( '/assets', express.static( path.join( __dirname, 'assets' )));
 app.use( '/', routes);
+app.use( compression());
 
 module.exports = app;
